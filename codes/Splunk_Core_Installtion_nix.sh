42c62dd3722e2ba3b988e92d8e797cf295943c04
SplunkAutobahnConsultants <92494749+SplunkAutobahnConsultants@users.noreply.github.com>
2021-10-23 12:28:01 -0400

    Update Splunk_Core_Installtion_nix.sh

--- codes/Splunk_Core_Installtion_nix.sh
+++ codes/Splunk_Core_Installtion_nix.sh
@@ -5,55 +5,32 @@
 # CentOS/RedHat installation Script - Splunk Core
 
 sudo yum install wget -y > ~/output.txt
-bk=`cat ~/output.txt |grep -i "already installed" |grep -o "wget is already installed"`
+bk=`cat ~/output.txt |grep -i "already installed" |grep -o "Git is already installed"`
  if [ "$bk" == "already installed" ]
                 then
                            echo "The wget package is $bk"
 
                            echo ""
 
-                           echo "We are ok to download the file using wget"
+                           echo "We are ok to download the file using git"
 
                            fi
 
-sudo wget -O splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm 'https://download.splunk.com/products/splunk/releases/8.2.2.1/linux/splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm' /opt
+sudo wget -O splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm 'https://download.splunk.com/products/splunk/releases/8.2.2.1/linux/splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm'
 
-sudo rpm -i /opt/splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm
+mv splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm /opt/
+
+cd /opt/
+
+rpm -i splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm
 
 sudo /opt/splunk/bin/splunk start --accept-license
 
 sudo /opt/splunk/bin/splunk stop
 
-echo ""
-
-    sleep 5
-
-echo "Stopping Splunk to set boot start
-
-
-"
 sudo /opt/splunk/bin/splunk enable boot-start -user splunk
 
 sudo chown  -R splunk: /opt/splunk
 
-sleep 10
-
 sudo -H -u splunk /opt/splunk/bin/splunk start
-
-echo ""
-
-    sleep 5
-
-echo "Starting Splunk from Splunk user.
-
-
-
-"
-
-
-e=`/opt/splunk/bin/splunk version`
-echo "Splunk New Version $e"
-
-sudo -H -u splunk /opt/splunk/bin/splunk status
-
 
