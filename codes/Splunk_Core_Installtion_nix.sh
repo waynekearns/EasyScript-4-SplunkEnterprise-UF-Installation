79b2f0b94caf3482c06414fe6815524a4caf3f8f
SplunkAutobahnConsultants <92494749+SplunkAutobahnConsultants@users.noreply.github.com>
2021-10-24 23:45:05 -0400

    Update Splunk_Core_Installtion_nix.sh

--- codes/Splunk_Core_Installtion_nix.sh
+++ codes/Splunk_Core_Installtion_nix.sh
@@ -2,8 +2,6 @@
                                                     #Version_1.0.0 by BK
 
 # CentOS/RedHat installation Script - Splunk Core
-
-
 sudo wget -O splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm 'https://download.splunk.com/products/splunk/releases/8.2.2.1/linux/splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm'
 
 sudo mv splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm /opt/
@@ -11,6 +9,20 @@ sudo mv splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm /opt/
 cd /opt/
 
 rpm -i splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm 
+
+
+ #Ubuntu installation Script - Splunk Core
+: ' 
+sduo wget -O splunk-8.2.2.1-ae6821b7c64b-linux-2.6-amd64.deb 'https://download.splunk.com/products/splunk/releases/8.2.2.1/linux/splunk-8.2.2.1-ae6821b7c64b-linux-2.6-amd64.deb'
+
+sudo mv splunk-8.2.2.1-ae6821b7c64b-linux-2.6-amd64.deb /opt/
+
+cd /opt/
+
+dpkg -i splunk-8.2.2.1-ae6821b7c64b-linux-2.6-amd64.deb
+
+   '
+
 
 sudo /opt/splunk/bin/splunk start --accept-license
 
