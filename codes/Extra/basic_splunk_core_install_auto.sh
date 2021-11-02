6f648408d5c2499558b37cecb4f50769b18d8b28
SplunkAutobahnConsultants <92494749+SplunkAutobahnConsultants@users.noreply.github.com>
2021-11-02 12:15:51 -0400

    Create basic_splunk_core_install_auto.sh

---
+++ codes/Extra/basic_splunk_core_install_auto.sh
@@ -0,0 +1,17 @@
+# CentOS/RedHat installation Script - Splunk Core
+sudo wget -O splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm 'https://download.splunk.com/products/splunk/releases/8.2.2.1/linux/splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm'
+sudo mv splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm /opt/
+cd /opt/
+sudo rpm -i splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm
+
+
+sudo /opt/splunk/bin/splunk start --accept-license
+
+sudo /opt/splunk/bin/splunk stop
+
+sudo /opt/splunk/bin/splunk enable boot-start -user splunk
+sudo chown  -R splunk: /opt/splunk
+
+sudo -H -u splunk /opt/splunk/bin/splunk start
+
+sudo -H -u splunk /opt/splunk/bin/splunk status
