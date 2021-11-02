2c7ca3c248ee3c5e5e5900a30d41e5f8f2b4a647
SplunkAutobahnConsultants <92494749+SplunkAutobahnConsultants@users.noreply.github.com>
2021-11-02 12:33:25 -0400

    Update basic_splunk_core_install_auto.sh

--- codes/Extra/basic_splunk_core_install_auto.sh
+++ codes/Extra/basic_splunk_core_install_auto.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+                                                               #Version_1.0.0 by Bisrat Kebede, BK
+
+
 # CentOS/RedHat installation Script - Splunk Core
 sudo wget -O splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm 'https://download.splunk.com/products/splunk/releases/8.2.2.1/linux/splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm'
 sudo mv splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm /opt/
